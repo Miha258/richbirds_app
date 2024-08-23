905f0ce8b19d8324ef3394e14c5377841a2d11ac
Miha258 <rizoks29@gmail.com>
2024-08-23 16:53:06 +0300

    m

--- bird-game-app/src/Deposit/Rechange.tsx
+++ bird-game-app/src/Deposit/Rechange.tsx
@@ -12,7 +12,7 @@ const Recharge = () => {
             <button style={{ padding: '0.4rem 1.3rem 0.4rem'}} className={styles.copyBtn}>Copy</button>
         </div>
         <p className={styles.rechargeInfo}>Please do not recharge other non-TRC20 (USDT) assets. The funds will arrive in your account in about 1 to 3 minutes after recharging.</p>
-        <p style={{ color: 'black', fontSize: '21px'}}>1000 GOLD = 10 USDT</p>
+        <p style={{ color: 'black', fontSize: '21px'}}><strong>1000 GOLD = 10 USDT</strong></p>
       </div>
       <button className={styles.rechargeBtn}>Recharge</button>
     </div>
